b0e0bd3053e240bbfe723ca326a5e81296519727
guga56 <gustavogregorio086@gmail.com>
2023-05-06 13:55:14 -0300

    criar a classe serviço e a interface e criei a classe loginView

---
+++ PortalEstudo.Servico/Servico/Interface/ILoginServico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalEstudo.Servico.Servico.Interface
+{
+    internal interface ILoginServico
+    {
+    }
+}
---
+++ PortalEstudo.Servico/Servico/LoginServico.cs
@@ -0,0 +1,21 @@
+﻿using PortalEstudo.Data.Repositorio.Interface;
+using PortalEstudo.Dominio.Dominio;
+using PortalEstudo.Servico.Servico.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalEstudo.Servico.Servico
+{
+    public class LoginServico : ILoginServico
+    {
+        private readonly ILoginRepository _reepositorio;
+
+        public void Adicionar(Login login)
+        {
+            _reepositorio.Inserir(login);
+        }
+    }
+}
