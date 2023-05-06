466a86c9a4a744b718cea84a7ce06f942c8e867b
guga56 <gustavogregorio086@gmail.com>
2023-05-05 21:30:31 -0300

    criara o logindao como meotodo salvar

---
+++ PortalEstudo.Data/Persistence/LoginDao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PortalEstudo.Dominio.Dominio;
+
+namespace PortalEstudo.Data.Persistence
+{
+    public class LoginDao : Conexao
+    {
+        public void Salvar(Login login)
+        {
+            Open();
+            Cmd = new SqlCommand("insert into Login values(@v1, @v2, @v3)");
+            Cmd.Parameters.AddWithValue("@v1", login.Usuario);
+            Cmd.Parameters.AddWithValue("@v2", login.Senha);
+            Cmd.Parameters.AddWithValue("@v3", login.Perfil);
+            Cmd.ExecuteNonQuery();
+            Close();
+        }
+    }
+}
